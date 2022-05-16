204c612d25a1e826a4ea9436732f9962d47e7f58
bhaskarreddy <bhaskrrd10@gmail.com>
2022-05-16 19:04:47 +0530

    added quiz_brain class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Queastion
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -8,4 +9,7 @@ for question in question_data:
     new_question = Queastion(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank)
+quiz =  QuizBrain(question_bank)
+quiz.next_question()
+
+
